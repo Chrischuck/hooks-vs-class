ccbf166ead2b33e8762e8b3dde04a0d01ac0b22e
Chris <chrischuck35@gmail.com>
2019-05-16 10:32:58 -0700

    Reverts to single state

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -4,16 +4,11 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-
 
   function onClick() {
     setCount(count + 1)
   }
 
-  function onClick2() {
-    setCount2(count2 + 1)
-  }
 
   return (
     <div style={{marginLeft: '20px'}}>
@@ -21,10 +16,6 @@ const App = () => {
         { count }
       </p>
       <button onClick={onClick}>Click Me!</button>
-      <p>
-        { count2 }
-      </p>
-      <button onClick={onClick2s}>Click Me!</button>
     </div>
   )
 }
