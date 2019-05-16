8125e30b9e216822b006665dc2041eefa2bf9b4b
Chris <chrischuck35@gmail.com>
2019-05-16 09:58:23 -0700

    Adds counter state to component folder

--- component/src/index.js
+++ component/src/index.js
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      counter: 1
+    }
+  }
+
+  onClick = () => {
+    this.setState((prevState) => ({ counter: prevState.counter + 1 }))
+  }
+
   render() {
     return (
       <div>
+        { this.state.counter }
+        <button onClick={this.onClick}>Click Me!</button>
       </div>
     )
   }
